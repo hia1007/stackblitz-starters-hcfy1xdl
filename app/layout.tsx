a7271ae39d5316328fcda7d2a639dcc09c27e050
shahriar1007 <2221141152@student.bup.edu.bd>
2026-06-28 20:38:18 +0600

    Wrap app with NextAuth SessionProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import './globals.css';
+import SessionProviders from './providers/SessionProviderClient';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-50 min-h-screen pb-24"> {/* Add padding-bottom so content isn't hidden behind the dock */}
-        <main className="p-4">
-          {children}
-        </main>
-        
+        <SessionProviders>
+          <main className="p-4">
+            {children}
+          </main>
+        </SessionProviders>
       </body>
     </html>
   );
