4a316f798124d4beaf2e233766fa910169917e1a
shahriar1007 <2221141152@student.bup.edu.bd>
2026-06-28 20:49:58 +0600

    Remove SessionProvider wrapper from layout (switch to simple layout)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import './globals.css';
-import SessionProviders from './providers/SessionProviderClient';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-50 min-h-screen pb-24"> {/* Add padding-bottom so content isn't hidden behind the dock */}
-        <SessionProviders>
-          <main className="p-4">
-            {children}
-          </main>
-        </SessionProviders>
+        <main className="p-4">
+          {children}
+        </main>
       </body>
     </html>
   );
